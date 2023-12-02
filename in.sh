495308bd420a67329e26ea36b81ad400019d0d82
Joseph Wang <josephwang@letu.edu>
2023-12-02 14:02:40 -0800

    Refactored util

--- in.sh
+++ in.sh
@@ -1,8 +1,8 @@
 # Specify day as command line argument
 # ./util 1
 
-py get_input.py $1
-py open_page.py $1
+py util/get_input.py $1
+py util/open_page.py $1
 # cp template.py $1.py
 code $1.in
 # code $1.py
