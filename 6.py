e9211083d34bc1af2500b10c277cac228ba9550e
Joseph Wang <josephwang@letu.edu>
2023-12-05 21:13:23 -0800

    Day 6

--- 6.py
+++ 6.py
@@ -1,16 +1,24 @@
 from utils import * # Counter, defaultdict, deque, deepcopy, cache (@cache), tqdm, math, sys, also gok(grid,r,c), dirs, adjs, letters, digits, symbols
 
 
-def part1(lines):
-    # lines = ints(lines)
-    # lines = words(lines)
-    # g = grid(lines)
-    
-    # tot = 0
-    for l in lines:
-        pass
-    return
+# !! I got on the global leaderboard!!!!!
+# 135/42
+# I was honestly losing hope. My earliest top performance was rank 644, so I wasn't even sure if my goal was still realistic.
 
+def part1(lines):
+    lines = ints(lines)
+    ts = lines[0]
+    dists = lines[1]
+    res = 1
+    for i,t in enumerate(ts):
+        tot = 0
+        for hold in range(t):
+            if (t-hold)*hold > dists[i]:
+                tot += 1
+        res *= tot
+    return res
+
+# !! I just manually modified the input
 def part2(lines):
     return
 
---
+++ 7.py
@@ -0,0 +1,27 @@
+from utils import * # Counter, defaultdict, deque, deepcopy, cache (@cache), tqdm, math, sys, also gok(grid,r,c), dirs, adjs, letters, digits, symbols
+
+
+def part1(lines):
+    # lines = ints(lines)
+    # lines = words(lines)
+    # g = grid(lines)
+    
+    # tot = 0
+    for l in lines:
+        pass
+    return
+
+def part2(lines):
+    return
+
+
+day = path.splitext(path.basename(__file__))[0]
+ls = [l.strip() for l in open(f'{day}.in')]
+p2 = part2(ls)
+if p2:
+    print(p2)
+    pyperclip.copy(p2)
+if not p2:
+    p1 = part1(ls)
+    print(p1)
+    pyperclip.copy(p1)
