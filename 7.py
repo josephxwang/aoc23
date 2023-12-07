f5514266d240fc1edb5bca58e8c5c2db2fbb901f
Joseph Wang <josephwang@letu.edu>
2023-12-06 22:12:50 -0800

    Day 7 refactoring

--- 7.py
+++ 7.py
@@ -3,172 +3,104 @@ from utils import * # Counter, defaultdict, deque, deepcopy, cache (@cache), tqd
 # A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, or 2
 
 def part1(lines):
-    tot = 0
-    fives = set()
-    fours = set()
-    fulls = set()
-    threes = set()
-    twos = set()
-    ones = set()
-    highs = set()
-    
     bids = {}
+    scores = []
     for l in lines:
         l = l.replace('A','E').replace('K','D').replace('Q','C').replace('J','B').replace('T','A')
-        cards, bid = l.split()
-        bids[cards] = int(bid)
+        hand, bid = l.split()
+        bids[hand] = int(bid)
         
-        counts = [e[1] for e in Counter(cards).most_common()]
+        counts = [e[1] for e in Counter(hand).most_common()]
         if counts == [5]:
-            fives.add(cards)
+            scores.append((6,hand))
         elif counts == [4,1]:
-            fours.add(cards)
+            scores.append((5,hand))
         elif counts == [3,2]:
-            fulls.add(cards)
+            scores.append((4,hand))
         elif counts == [3,1,1]:
-            threes.add(cards)
+            scores.append((3,hand))
         elif counts == [2,2,1]:
-            twos.add(cards)
+            scores.append((2,hand))
         elif counts == [2,1,1,1]:
-            ones.add(cards)
-        else:
-            highs.add(cards)
-        # s = set(cards)
-        # if len(s) == 1:
-        #     fives.add(cards)
-        # elif len(s) == 2:
-        #     for card in s:
-        #         if cards.count(card) == 4:
-        #             fours.add(cards)
-        #         elif cards.count(card) == 3:
-        #             fulls.add(cards)
-        # elif len(s) == 3:
-        #     for card in s:
-        #         if cards.count(card) == 3:
-        #             threes.add(cards)
-        #         elif cards.count(card) == 2:
-        #             twos.add(cards)
-        # elif len(s) == 4:
-        #     ones.add(cards)
-        # elif len(s) == 5:
-        #     highs.add(cards)
+            scores.append((1,hand))
+        elif counts == [1,1,1,1,1]:
+            scores.append((0,hand))
             
-    fives = list(fives)
-    fours = list(fours)
-    fulls = list(fulls)
-    threes = list(threes)
-    twos = list(twos)
-    ones = list(ones)
-    highs = list(highs)
-    
-    fives.sort(reverse=True); fours.sort(reverse=True); fulls.sort(reverse=True); threes.sort(reverse=True); twos.sort(reverse=True); ones.sort(reverse=True); highs.sort(reverse=True)
-        
-    rank = len(lines)
-    for cards in fives:
-        tot += bids[cards] * rank
-        rank -= 1
-    for cards in fours:
-        tot += bids[cards] * rank
-        rank -= 1
-    for cards in fulls:
-        tot += bids[cards] * rank
-        rank -= 1
-    for cards in threes:
-        tot += bids[cards] * rank
-        rank -= 1
-    for cards in twos:
-        tot += bids[cards] * rank
-        rank -= 1
-    for cards in ones:
-        tot += bids[cards] * rank
-        rank -= 1
-    for cards in highs:
-        tot += bids[cards] * rank
-        rank -= 1
-    
+    rank = 1
+    tot = 0
+    for _,hand in sorted(scores):
+        tot += bids[hand] * rank
+        rank += 1
+            
     return tot
 
 def part2(lines):
-    return
-    tot = 0
-    fives = set()
-    fours = set()
-    fulls = set()
-    threes = set()
-    twos = set()
-    ones = set()
-    highs = set()
+    # return
+    fives = []
+    fours = []
+    fulls = []
+    threes = []
+    twos = []
+    ones = []
+    highs = []
     
     bids = {}
     for l in lines:
         l = l.replace('A','E').replace('K','D').replace('Q','C').replace('J','1').replace('T','A')
-        cards, bid = l.split()
-        orig = cards
+        hand, bid = l.split()
+        orig = hand
         
-        counts = Counter(cards.replace('1',''))
+        bids[orig] = int(bid)
+        
+        counts = Counter(hand.replace('1',''))
         if len(counts) > 0:
             most_common, count = counts.most_common(1)[0]
         else:
             most_common = '1'
         if count == 1:
-            most_common = sorted(cards)[-1]
-        for i,c in enumerate(deepcopy(cards)):
+            most_common = sorted(hand)[-1]
+        for i,c in enumerate(deepcopy(hand)):
             if c == '1':
-                cards = cards[:i] + most_common + cards[i+1:]
+                hand = hand[:i] + most_common + hand[i+1:]
         
-        s = set(cards)
-        if len(s) == 1:
-            fives.add(orig)
-        elif len(s) == 2:
-            for card in s:
-                if cards.count(card) == 4:
-                    fours.add(orig)
-                elif cards.count(card) == 3:
-                    fulls.add(orig)
-        elif len(s) == 3:
-            for card in s:
-                if cards.count(card) == 3:
-                    threes.add(orig)
-                elif cards.count(card) == 2:
-                    twos.add(orig)
-        elif len(s) == 4:
-            ones.add(orig)
-        elif len(s) == 5:
-            highs.add(orig)
-        
-        bids[orig] = int(bid)
-            
-    fives = list(fives)
-    fours = list(fours)
-    fulls = list(fulls)
-    threes = list(threes)
-    twos = list(twos)
-    ones = list(ones)
-    highs = list(highs)
-    
-    fives.sort(reverse=True); fours.sort(reverse=True); fulls.sort(reverse=True); threes.sort(reverse=True); twos.sort(reverse=True); ones.sort(reverse=True); highs.sort(reverse=True)
-    
+        counts = [e[1] for e in Counter(hand).most_common()]
+        if counts == [5]:
+            fives.append(orig)
+        elif counts == [4,1]:
+            fours.append(orig)
+        elif counts == [3,2]:
+            fulls.append(orig)
+        elif counts == [3,1,1]:
+            threes.append(orig)
+        elif counts == [2,2,1]:
+            twos.append(orig)
+        elif counts == [2,1,1,1]:
+            ones.append(orig)
+        else:
+            highs.append(orig)
+                
     rank = len(lines)
-    for cards in fives:
-        tot += bids[cards] * rank
+    tot = 0
+    for hand in sorted(fives,reverse=True):
+        tot += bids[hand] * rank
         rank -= 1
-    for cards in fours:
-        tot += bids[cards] * rank
+    for hand in sorted(fours,reverse=True):
+        tot += bids[hand] * rank
         rank -= 1
-    for cards in fulls:
-        tot += bids[cards] * rank
+    for hand in sorted(fulls,reverse=True):
+        tot += bids[hand] * rank
         rank -= 1
-    for cards in threes:
-        tot += bids[cards] * rank
+    for hand in sorted(threes,reverse=True):
+        tot += bids[hand] * rank
         rank -= 1
-    for cards in twos:
-        tot += bids[cards] * rank
+    for hand in sorted(twos,reverse=True):
+        tot += bids[hand] * rank
         rank -= 1
-    for cards in ones:
-        tot += bids[cards] * rank
+    for hand in sorted(ones,reverse=True):
+        tot += bids[hand] * rank
         rank -= 1
-    for cards in highs:
-        tot += bids[cards] * rank
+    for hand in sorted(highs,reverse=True):
+        tot += bids[hand] * rank
         rank -= 1
     
     return tot
