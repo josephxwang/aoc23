14c5b814e2444397e3793825c395261cfe7339a9
Joseph Wang <josephwang@letu.edu>
2023-12-06 22:43:43 -0800

    Day 8 prep

--- 7.py
+++ 7.py
@@ -6,103 +6,57 @@ def part1(lines):
     bids = {}
     scores = []
     for l in lines:
-        l = l.replace('A','E').replace('K','D').replace('Q','C').replace('J','B').replace('T','A')
-        hand, bid = l.split()
+        hand,bid = l.split()
+        hand = hand.replace('A','E').replace('K','D').replace('Q','C').replace('J','B').replace('T','A')
         bids[hand] = int(bid)
         
         counts = [e[1] for e in Counter(hand).most_common()]
-        if counts == [5]:
-            scores.append((6,hand))
-        elif counts == [4,1]:
-            scores.append((5,hand))
-        elif counts == [3,2]:
-            scores.append((4,hand))
-        elif counts == [3,1,1]:
-            scores.append((3,hand))
-        elif counts == [2,2,1]:
-            scores.append((2,hand))
-        elif counts == [2,1,1,1]:
-            scores.append((1,hand))
-        elif counts == [1,1,1,1,1]:
-            scores.append((0,hand))
-            
-    rank = 1
+        score = [[1,1,1,1,1],[2,1,1,1],[2,2,1],[3,1,1],[3,2],[4,1],[5]].index(counts)
+        scores.append((score,hand))
+
     tot = 0
+    rank = 1
     for _,hand in sorted(scores):
-        tot += bids[hand] * rank
+        tot += bids[hand]*rank
         rank += 1
-            
     return tot
 
 def part2(lines):
     # return
-    fives = []
-    fours = []
-    fulls = []
-    threes = []
-    twos = []
-    ones = []
-    highs = []
-    
     bids = {}
+    scores = []
     for l in lines:
-        l = l.replace('A','E').replace('K','D').replace('Q','C').replace('J','1').replace('T','A')
-        hand, bid = l.split()
+        hand,bid = l.split()
+        hand = hand.replace('A','E').replace('K','D').replace('Q','C').replace('J','1').replace('T','A')
+        bids[hand] = int(bid)
+        
         orig = hand
-        
-        bids[orig] = int(bid)
-        
-        counts = Counter(hand.replace('1',''))
-        if len(counts) > 0:
-            most_common, count = counts.most_common(1)[0]
-        else:
-            most_common = '1'
-        if count == 1:
-            most_common = sorted(hand)[-1]
-        for i,c in enumerate(deepcopy(hand)):
-            if c == '1':
-                hand = hand[:i] + most_common + hand[i+1:]
+        if hand != '11111': # except for all J card instance, JJJJJ
+            card,_ = Counter(hand.replace('1','')).most_common()[0] # find most common non-J card
+            hand = hand.replace('1',card)
         
         counts = [e[1] for e in Counter(hand).most_common()]
-        if counts == [5]:
-            fives.append(orig)
-        elif counts == [4,1]:
-            fours.append(orig)
-        elif counts == [3,2]:
-            fulls.append(orig)
-        elif counts == [3,1,1]:
-            threes.append(orig)
-        elif counts == [2,2,1]:
-            twos.append(orig)
-        elif counts == [2,1,1,1]:
-            ones.append(orig)
-        else:
-            highs.append(orig)
-                
-    rank = len(lines)
-    tot = 0
-    for hand in sorted(fives,reverse=True):
-        tot += bids[hand] * rank
-        rank -= 1
-    for hand in sorted(fours,reverse=True):
-        tot += bids[hand] * rank
-        rank -= 1
-    for hand in sorted(fulls,reverse=True):
-        tot += bids[hand] * rank
-        rank -= 1
-    for hand in sorted(threes,reverse=True):
-        tot += bids[hand] * rank
-        rank -= 1
-    for hand in sorted(twos,reverse=True):
-        tot += bids[hand] * rank
-        rank -= 1
-    for hand in sorted(ones,reverse=True):
-        tot += bids[hand] * rank
-        rank -= 1
-    for hand in sorted(highs,reverse=True):
-        tot += bids[hand] * rank
-        rank -= 1
+        match counts:
+            case [5]:
+                scores.append((6,orig))
+            case [4,1]:
+                scores.append((5,orig))
+            case [3,2]:
+                scores.append((4,orig))
+            case [3,1,1]:
+                scores.append((3,orig))
+            case [2,2,1]:
+                scores.append((2,orig))
+            case [2,1,1,1]:
+                scores.append((1,orig))
+            case [1,1,1,1,1]:
+                scores.append((0,orig))
     
+    tot = 0
+    rank = 1
+    for _,hand in sorted(scores):
+        tot += bids[hand]*rank
+        rank += 1    
     return tot
 
 
