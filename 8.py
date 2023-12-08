6eb334d77d79a86fb373eeb1e42e9877f97230d5
Joseph Wang <josephwang@letu.edu>
2023-12-08 02:22:53 -0800

    Day 8

--- 8.py
+++ 8.py
@@ -2,17 +2,58 @@ from utils import * # Counter, defaultdict, deque, deepcopy, cache (@cache), mat
 
 
 def part1(lines):
-    # lines = ints(lines)
-    # lines = words(lines)
-    # g = grid(lines)
-    
-    # tot = 0
-    for l in lines:
-        pass
-    return
+    insts = lines[0]
+    lines = lines[2:]
+    m = {}
+    for line in lines:
+        node,l,r = line.split()
+        m[node] = (l,r)
+    s = 'AAA'
+    for i in range(100000):
+        inst = insts[i%len(insts)]
+        if s == 'ZZZ':
+            return i
+        if inst == 'R':
+            s = m[s][1]
+        elif inst == 'L':
+            s = m[s][0]
 
 def part2(lines):
-    return
+    # return
+    insts = lines[0]
+    lines = lines[2:]
+    m = {}
+    nodes = []
+    for line in lines:
+        node,l,r = line.split()
+        m[node] = (l,r)
+        if node.endswith('A'):
+            nodes.append(node)
+    firsts = defaultdict(int)
+    for i in range(100000):
+        inst = insts[i%len(insts)]
+        for j,node in enumerate(deepcopy(nodes)):
+            if node.endswith('Z'):
+                if firsts[j] == 0:
+                    firsts[j] = i
+            if inst == 'R':
+                nodes[j] = m[node][1]
+            elif inst == 'L':
+                nodes[j] = m[node][0]
+    return math.lcm(*firsts.values()) # for some reason the input works with just lcm
+
+    # for i in range(1,100000000000):
+    #     ok = True
+    #     for node,dests in firsts.items():
+    #         ok_one = False
+    #         for node,data in dests.items():
+    #             if (i-data[0]) % data[1] == 0: # apparently you can also use Chinese remainder theorem?
+    #                 ok_one = True
+    #                 break
+    #         if not ok_one:
+    #             ok = False
+    #     if ok:
+    #         return i
 
 
 day = path.splitext(path.basename(__file__))[0]
